fab2b1eb671673425b2071b42713a751ec0925a4
Mehdi Lavasani <mehdilavasani79@gmail.com>
2024-04-10 08:15:57 +0330

    JList tools

---
+++ src/org/zendev/lib/gui/list/JListUtils.java
@@ -0,0 +1,29 @@
+package org.zendev.lib.gui.list;
+
+import javax.swing.*;
+
+public class JListUtils {
+
+    public static <E> void clear(JList<E> list) {
+        DefaultListModel<E> model = (DefaultListModel<E>) list.getModel();
+        model.removeAllElements();
+
+        list.setModel(model);
+    }
+
+    public static <E> void add(JList<E> list, E item) {
+        ListModel<E> listModel = list.getModel();
+        DefaultListModel<E> defaultListModel = new DefaultListModel<>();
+
+        for (int j = 0; j < listModel.getSize(); j++) {
+            defaultListModel.addElement(listModel.getElementAt(j));
+        }
+
+        defaultListModel.addElement(item);
+        list.setModel(defaultListModel);
+    }
+
+    public static <E> void remove(JList<E> list, int index) {
+        ((DefaultListModel<E>) list.getModel()).remove(index);
+    }
+}
