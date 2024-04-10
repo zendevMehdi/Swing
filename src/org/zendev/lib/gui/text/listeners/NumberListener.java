8290cfc032c99d4cedb4149ee11da4264bdd9b30
Mehdi Lavasani <mehdilavasani79@gmail.com>
2024-04-10 08:17:46 +0330

    Only allow number to type into text component

---
+++ src/org/zendev/lib/gui/text/listeners/NumberListener.java
@@ -0,0 +1,24 @@
+package org.zendev.lib.gui.text.listeners;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class NumberListener implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (!StringUtils.isNumeric(String.valueOf(e.getKeyChar()))) {
+            e.consume();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+}
